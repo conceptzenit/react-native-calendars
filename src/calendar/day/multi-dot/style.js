6a8c3db32183d7765828ce6512cc27d63fd8c547
nikunj bansal <nikunj.iitd@gmail.com>
2020-03-06 10:20:28 -0500

    bigger dots multi-dot

--- src/calendar/day/multi-dot/style.js
+++ src/calendar/day/multi-dot/style.js
@@ -39,12 +39,12 @@ export default function styleConstructor(theme={}) {
       color: appStyle.textDisabledColor
     },
     dot: {
-      width: 4,
-      height: 4,
+      width: 8,
+      height: 8,
       marginTop: 1,
       marginLeft: 1,
       marginRight: 1,
-      borderRadius: 2,
+      borderRadius: 4,
       opacity: 0
     },
     visibleDot: {
